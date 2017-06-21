e51fb479ba515581f6a2a20809ad517a59211788
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-06-21 11:58:05 +0200

    Step 2.2

--- Asteroids/Asteroids/SpaceShip.cs
+++ Asteroids/Asteroids/SpaceShip.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,28 @@ namespace Asteroids
             startPos = new Vector2(Asteroids.Screen.X / 2, Asteroids.Screen.Y / 2);
             this.position = startPos;
             this.origin = sprite.Center;
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+
+            if (inputHelper.IsKeyDown(Keys.Up))
+            {
+                this.velocity += this.AngularDirection * 5;
+            }
+            if (inputHelper.IsKeyDown(Keys.Down))
+            {
+                this.velocity -= this.AngularDirection * 5;
+            }
+            if (inputHelper.IsKeyDown(Keys.Left))
+            {
+                this.Degrees -= 4;
+            }
+            if (inputHelper.IsKeyDown(Keys.Right))
+            {
+                this.Degrees += 4;
+            }
         }
     }
 }
