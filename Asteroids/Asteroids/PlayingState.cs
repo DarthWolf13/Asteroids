428214295ab7e85abd6323411f72531645beb484
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-06-21 15:00:36 +0200

    Step 4

--- Asteroids/Asteroids/PlayingState.cs
+++ Asteroids/Asteroids/PlayingState.cs
@@ -3,22 +3,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Asteroids
 {
     class PlayingState : GameObjectList
     {
         SpaceShip ship;
-        GameObjectList bullets;
+        GameObjectList bullets, rocks;
+        int rockAmount = 20;
+        string rockType;
 
         public PlayingState()
         {
             ship = new SpaceShip();
             bullets = new GameObjectList();
+            rocks = new GameObjectList();
+
+            string[] rock = new string[] {"spr_rock1", "spr_rock2", "spr_rock3"};
+            
 
             this.Add(new SpriteGameObject("spr_background"));
             this.Add(ship);
             this.Add(bullets);
+            this.Add(rocks);
+
+            for(int i = 0; i < rockAmount; i++)
+            {
+                rockType = rock[GameEnvironment.Random.Next(3)];
+                this.rocks.Add(new Rock(rockType));
+            }
         }
 
         public override void HandleInput(InputHelper inputHelper)
@@ -28,6 +42,39 @@ namespace Asteroids
             if (inputHelper.KeyPressed(Keys.Space))
             {
                 this.bullets.Add(new Bullet(ship.Position.X, ship.Position.Y, ship.AngularDirection * 300));
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            foreach(Bullet bullet in bullets.Objects)
+            {
+                foreach (Rock rock in rocks.Objects)
+                {
+                    if (bullet.CollidesWith(rock))
+                    {
+                        bullet.Visible = false;
+
+                        if (rock.assetname == "spr_rock3") {
+                            rock.Visible = false;
+                            this.rocks.Add(new Rock(rock.Position, rock.Velocity, "spr_rock2"));
+                            break;
+                        }
+                        else if(rock.assetname == "spr_rock2")
+                        {
+                            rock.Visible = false;
+                            this.rocks.Add(new Rock(rock.Position, rock.Velocity, "spr_rock1"));
+                            break;
+                        }
+                        else if(rock.assetname == "spr_rock1")
+                        {
+                            rock.Visible = false;
+                            break;
+                        }                        
+                    }
+                }
             }
         }
     }
---
+++ Asteroids/Asteroids/Rock.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Asteroids
+{
+    class Rock : SpriteGameObject
+    {
+        Vector2 randomPos, randomVel;
+        public string assetname;
+
+        public Rock(string assetname) : base(assetname)
+        {
+            randomPos = new Vector2(GameEnvironment.Random.Next(0, Asteroids.Screen.X), GameEnvironment.Random.Next(0, Asteroids.Screen.Y));
+            this.position = randomPos;
+            randomVel = new Vector2(GameEnvironment.Random.Next(1, 100), GameEnvironment.Random.Next(1, 100));
+            this.velocity = randomVel;
+            this.assetname = assetname;
+        }
+
+        public Rock(Vector2 location, Vector2 move, string assetname) : base(assetname)
+        {
+            //location = new Vector2();
+            this.position = location;
+            //move = new Vector2(0, 100);
+            this.velocity = move;
+            this.assetname = assetname;
+        }
+
+        public void WrapScreen()
+        {
+            if (this.position.X < 0)
+            {
+                this.position.X = Asteroids.Screen.X;
+            }
+
+            if (this.position.X > Asteroids.Screen.X)
+            {
+                this.position.X = 0;
+            }
+
+            if (this.position.Y < 0)
+            {
+                this.position.Y = Asteroids.Screen.Y;
+            }
+
+            if (this.position.Y > Asteroids.Screen.Y)
+            {
+                this.position.Y = 0;
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            WrapScreen();
+        }
+    }
+}
